6ea23d1a303b8acb89fdfcc956402c320ab2deb3
Joao Victor <victorjv3288@gmail.com>
2022-05-12 14:16:58 -0300

    Aula SQL
    Aula sobre conhecimentos básicos sobre SQL, com base nos conhecimentos do Jhonatan do canal Dev Aprender.

---
+++ SQL/query.sql
@@ -0,0 +1,22 @@
+SELECT DISTINCT coluna1,coluna2
+FROM tabela;
+/*
+ mostra nomes sem duplicidade na tabela
+*/
+
+-- WHERE
+SELECT coluna1,coluna2, coluna_n
+FROM tabela
+WHERE condição;
+
+/*
+OPERADOR    -    DESCRIÇÃO
+=                IGUAL
+>                MAIOR QUE
+<                MENOR QUE
+>=               MAIOR OU IGUAL
+<=               MENOR OU IGUAL
+<>               DIFERENTE DE
+AND              OPERADOR LÓGICO E
+OR               OPERADOR LÓGICO OU
+*/
