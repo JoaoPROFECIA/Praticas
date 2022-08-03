46401d2bbc523ba02a6ca6b1da951e66e5fb2f27
Joao Victor <victorjv3288@gmail.com>
2022-08-03 18:55:29 -0300

    Update sql-query-08-2022.sql

--- SQL/sql-query-08-2022.sql
+++ SQL/sql-query-08-2022.sql
@@ -87,3 +87,108 @@ WHERE BusinessEntityID = 26;
 
 -- peter0@adventure-works.com
 
+-- COUNT
+SELECT COUNT(*) FROM tabela;
+SELECT COUNT(DISTINCT coluna) FROM tabela;
+
+SELECT count(*)
+FROM Person.Person
+
+SELECT count(distinct Title)
+FROM Person.Person
+
+
+/*
+DESAFIO 6
+Quantos produtos temos cadastrados em nossa tabela de produtos?
+(Production.Product)
+*/
+SELECT COUNT(*)
+FROM Production.Product
+
+/*
+DESAFIO 7
+Quantos tamanhos de produtos temos cadastrados em nossa tabela?
+(Production.produtc)
+*/
+SELECT COUNT(SIZE)
+FROM Production.Product
+
+/*
+Quantos tamanhos diferentes de produtos temos cadastrados em nossa tabela?
+(Production.product)
+*/
+SELECT COUNT(distinct SIZE)
+FROM Production.Product
+
+
+-- TOP
+SELECT TOP 10 * FROM TABELA;
+
+SELECT TOP 10 * FROM Production.Product
+where color = 'blue';
+
+-- ORDER BY
+SELECT * FROM TABELA 
+ORDER BY coluna ASC, coluna_n DESC;
+
+SELECT *
+FROM Person.Person
+ORDER BY FirstName ASC, LastName desc
+
+SELECT FirstName, LastName
+FROM Person.Person
+ORDER BY FirstName ASC, LastName desc
+
+/*
+DESAFIO 8
+Obter o ProductID dos 10 produtos mais caros cadastrados na tabela, listando do mais caro para o mais barato.
+*/
+
+SELECT TOP 10 ProductID
+FROM Production.Product
+ORDER BY ListPrice desc
+
+/*
+DESAFIO 9
+Obter o nome e número dos produtos que tem o ProductID entre 1 e 4
+*/
+
+SELECT Name, ProductNumber
+FROM Production.Product
+WHERE ProductID BETWEEN 1 and 4
+
+SELECT TOP 4 Name, ProductNumber
+FROM Production.Product
+ORDER BY ProductID asc
+
+
+
+-- BETWEEN
+É usado para selecionar os valores entre um intervalo de valores mínimo e máximo.
+
+Ex: 
+WHERE ProductID BETWEEN 1 and 4
+
+SELECT * FROM Production.Product
+WHERE ListPrice BETWEEN 1000 and 1500
+
+SELECT * FROM Production.Product
+WHERE ListPrice not BETWEEN 1000 and 1500
+
+SELECT * FROM HumanResources.Employee
+WHERE HireDate between '2009-01-01' and '2010-01-01'
+ORDER BY HireDate
+
+-- IN
+É usado para selecionar os valores que estão dentro de uma lista de valores.
+
+Ex: 
+WHERE ProductID IN (1,2,3,4)
+OU
+WHERE ProductID IN (SELECT ProductID FROM Production.Product WHERE Color = 'blue')
+
+SELECT *
+FROM Person.Person
+WHERE BusinessEntityID IN (2,7,13)
+
