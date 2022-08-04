1847ad754e4ed042bb683e639e3556b4b23ae464
Joao Victor <victorjv3288@gmail.com>
2022-08-03 21:43:26 -0300

    Update sql-query-08-2022.sql

--- SQL/sql-query-08-2022.sql
+++ SQL/sql-query-08-2022.sql
@@ -1,10 +1,10 @@
-SELECT
+-- SELECT
 -- SQL Server, PostgreSQL, MySQL, SQLite and Oracle
 
-SELECT colunm1,colunm2
-FROM table
+    SELECT colunm1,colunm2
+    FROM table
 
-SELECT * FROM table
+    SELECT * FROM table
 
 /*
 DESAFIO 1
@@ -14,26 +14,26 @@ e precisa do nome e sobrenome de todos os clientes cadastrados no sistema.
 first name, last name
 */
 
-SELECT firstname, lastname FROM Person.Person;
+    SELECT firstname, lastname FROM Person.Person;
 
 /*
 DESAFIO 2
 Quantos sobrenomes únicos temos na tabela Person.Person?
 */
-SELECT DISTINCT LastName 
-FROM Person.Person;
+    SELECT DISTINCT LastName 
+    FROM Person.Person;
 
-SELECT DISTINCT coluna1, coluna2 
-FROM tabela;
+    SELECT DISTINCT coluna1, coluna2 
+    FROM tabela;
 
-SELECT DISTINCT FirstName 
-FROM Person.Person;
+    SELECT DISTINCT FirstName 
+    FROM Person.Person;
 
 
 -- WHERE
-SELECT coluna1,coluna2,coluna_n
-FROM tabela
-WHERE condição;
+    SELECT coluna1,coluna2,coluna_n
+    FROM tabela
+    WHERE condição;
 
 /*
 OPERADOR    -    DESCRIÇÃO
@@ -47,10 +47,10 @@ AND              OPERADOR LÓGICO E
 OR               OPERADOR LÓGICO OU
 */
 
-SELECT * FROM Production.Product
-where ListPrice > 1500 and ListPrice < 5000
-where Color = 'blue' or Color = 'black'
-where color <> 'red'
+    SELECT * FROM Production.Product
+    where ListPrice > 1500 and ListPrice < 5000
+    where Color = 'blue' or Color = 'black'
+    where color <> 'red'
 
 -- DESAFIO 3
 /*
@@ -59,8 +59,8 @@ e não mais que 700kg para inspeção
 */
 --weight > 500 and weight < 700
 
-SELECT * FROM Production.Product
-WHERE weight > 500 and weight <= 700
+    SELECT * FROM Production.Product
+    WHERE weight > 500 and weight <= 700
 
 -- DESAFIO 4
 /*
@@ -68,8 +68,8 @@ Foi solicitado pela equipe de marketing uma relação de todos os empregados (em
 (single / married) e que são assalariados (salaried).
 */
 
-SELECT * FROM HumanResources.Employee
-WHERE MaritalStatus = 'M' and salariedflag = 1;
+    SELECT * FROM HumanResources.Employee
+    WHERE MaritalStatus = 'M' and salariedflag = 1;
 
 -- DESAFIO 5
 /*
@@ -78,24 +78,24 @@ uma cobrança.
 Tabelas: Person.Person e Person.EmailAddress
 */
 
-SELECT * FROM Person.Person
-WHERE FirstName = 'Peter' and LastName = 'Krebs';
+    SELECT * FROM Person.Person
+    WHERE FirstName = 'Peter' and LastName = 'Krebs';
 
-SELECT * 
-FROM Person.EmailAddress
-WHERE BusinessEntityID = 26;
+    SELECT * 
+    FROM Person.EmailAddress
+    WHERE BusinessEntityID = 26;
 
 -- peter0@adventure-works.com
 
 -- COUNT
-SELECT COUNT(*) FROM tabela;
-SELECT COUNT(DISTINCT coluna) FROM tabela;
+    SELECT COUNT(*) FROM tabela;
+    SELECT COUNT(DISTINCT coluna) FROM tabela;
 
-SELECT count(*)
-FROM Person.Person
+    SELECT count(*)
+    FROM Person.Person
 
-SELECT count(distinct Title)
-FROM Person.Person
+    SELECT count(distinct Title)
+    FROM Person.Person
 
 
 /*
@@ -103,42 +103,46 @@ DESAFIO 6
 Quantos produtos temos cadastrados em nossa tabela de produtos?
 (Production.Product)
 */
-SELECT COUNT(*)
-FROM Production.Product
+
+    SELECT COUNT(*)
+    FROM Production.Product
 
 /*
 DESAFIO 7
 Quantos tamanhos de produtos temos cadastrados em nossa tabela?
 (Production.produtc)
 */
-SELECT COUNT(SIZE)
-FROM Production.Product
+
+    SELECT COUNT(SIZE)
+    FROM Production.Product
 
 /*
 Quantos tamanhos diferentes de produtos temos cadastrados em nossa tabela?
 (Production.product)
 */
-SELECT COUNT(distinct SIZE)
-FROM Production.Product
 
+    SELECT COUNT(distinct SIZE)
+    FROM Production.Product
 
 -- TOP
-SELECT TOP 10 * FROM TABELA;
 
-SELECT TOP 10 * FROM Production.Product
-where color = 'blue';
+    SELECT TOP 10 * FROM TABELA;
+
+    SELECT TOP 10 * FROM Production.Product
+    where color = 'blue';
 
 -- ORDER BY
-SELECT * FROM TABELA 
-ORDER BY coluna ASC, coluna_n DESC;
 
-SELECT *
-FROM Person.Person
-ORDER BY FirstName ASC, LastName desc
+    SELECT * FROM TABELA 
+    ORDER BY coluna ASC, coluna_n DESC;
 
-SELECT FirstName, LastName
-FROM Person.Person
-ORDER BY FirstName ASC, LastName desc
+    SELECT *
+    FROM Person.Person
+    ORDER BY FirstName ASC, LastName desc
+
+    SELECT FirstName, LastName
+    FROM Person.Person
+    ORDER BY FirstName ASC, LastName desc
 
 /*
 DESAFIO 8
@@ -154,41 +158,188 @@ DESAFIO 9
 Obter o nome e número dos produtos que tem o ProductID entre 1 e 4
 */
 
-SELECT Name, ProductNumber
-FROM Production.Product
-WHERE ProductID BETWEEN 1 and 4
+    SELECT Name, ProductNumber
+    FROM Production.Product
+    WHERE ProductID BETWEEN 1 and 4
 
-SELECT TOP 4 Name, ProductNumber
-FROM Production.Product
-ORDER BY ProductID asc
-
+    SELECT TOP 4 Name, ProductNumber
+    FROM Production.Product
+    ORDER BY ProductID asc
 
 
 -- BETWEEN
-É usado para selecionar os valores entre um intervalo de valores mínimo e máximo.
+-- É usado para selecionar os valores entre um intervalo de valores mínimo e máximo.
 
-Ex: 
-WHERE ProductID BETWEEN 1 and 4
+-- Ex: 
+    WHERE ProductID BETWEEN 1 and 4
 
-SELECT * FROM Production.Product
-WHERE ListPrice BETWEEN 1000 and 1500
+    SELECT * FROM Production.Product
+    WHERE ListPrice BETWEEN 1000 and 1500
 
-SELECT * FROM Production.Product
-WHERE ListPrice not BETWEEN 1000 and 1500
+    SELECT * FROM Production.Product
+    WHERE ListPrice not BETWEEN 1000 and 1500
 
-SELECT * FROM HumanResources.Employee
-WHERE HireDate between '2009-01-01' and '2010-01-01'
-ORDER BY HireDate
+    SELECT * FROM HumanResources.Employee
+    WHERE HireDate between '2009-01-01' and '2010-01-01'
+    ORDER BY HireDate
 
 -- IN
-É usado para selecionar os valores que estão dentro de uma lista de valores.
+-- É usado para selecionar os valores que estão dentro de uma lista de valores.
 
-Ex: 
-WHERE ProductID IN (1,2,3,4)
-OU
-WHERE ProductID IN (SELECT ProductID FROM Production.Product WHERE Color = 'blue')
+-- Ex: 
+    WHERE ProductID IN (1,2,3,4)
+-- OU
+    WHERE ProductID IN (SELECT ProductID FROM Production.Product WHERE Color = 'blue')
 
-SELECT *
-FROM Person.Person
-WHERE BusinessEntityID IN (2,7,13)
+    SELECT *
+    FROM Person.Person
+    WHERE BusinessEntityID IN (2,7,13)
+
+-- LIKE
+-- É usado para selecionar os valores que contém o texto especificado.
+
+    Ex: 
+    SELECT *
+    FROM Person.Person
+    WHERE FirstName LIKE 'ovi%'
+-- OU
+    WHERE FirstName LIKE '%to'
+-- OU
+    WHERE FirstName LIKE '%essa%'
+
+-- DESAFIO 10
+/*
+Quantos produtos temos cadastrados no sistema que custam mais que 1500?
+(Production.Product)
+*/
+
+    SELECT COUNT(*)
+    FROM Production.Product
+    WHERE ListPrice > 1500
+
+    SELECT COUNT(listprice)
+    FROM Production.Product
+    WHERE ListPrice > 1500
+
+-- DESAFIO 11
+/*
+Quantas pessoas temos com o sobrenome que inicia com a letra P?
+(Person.Person)
+*/
+
+    SELECT COUNT(LastName) FROM Person.Person WHERE LastName LIKE 'P%'
+
+-- DESAFIO 12
+/*
+Em quantas cidades únicas estão cadastradas nossos clientes?
+(Person.Address)
+*/
+
+    SELECT COUNT(DISTINCT City) FROM Person.Address
+
+-- DESAFIO 13
+/*
+Quais são as cidades únicas que estão cadastradas em nossos sistemas?
+(Person.Address)
+*/
+
+    SELECT DISTINCT City
+    FROM Person.Address
+    ORDER BY City
+
+-- DESAFIO 14
+/*
+Quantos produtos vermelhos tem o preco entre 500 e 1000 dolares?
+(Production.Product)
+*/
+
+    SELECT COUNT(*)
+    FROM Production.Product
+    WHERE Color = 'red' and
+    ListPrice BETWEEN 500 and 1000
+
+-- DESAFIO 15
+/*
+Quantos produtos cadastrados tem a palavra 'road' no nome?
+(Production.Product)
+*/
+
+    SELECT COUNT(*)
+    FROM Production.Product
+    WHERE Name LIKE '%road%'
+
+-- MIN, MAX, SUM and AVG
+    SELECT MIN(ListPrice) FROM Production.Product
+
+    SELECT TOP 10 sum(linetotal) as 'Soma'
+    FROM Sales.SalesOrderDetail
+
+    SELECT TOP 10 min(linetotal) as 'Valor mínimo'
+    FROM Sales.SalesOrderDetail
+
+    SELECT TOP 10 avg(linetotal) as 'Preço médio'
+    FROM Sales.SalesOrderDetail
+
+-- Group By
+/*
+Vai dividir os resultados em grupos e agrupar os resultados.
+*/
+
+    SELECT coluna 
+    FROM tabela
+    GROUP BY coluna
+
+    SELECT *
+    FROM Sales.SalesOrderDetail
+
+    SELECT SpecialOfferID, SUM(UnitPrice) as 'Soma'
+    FROM Sales.SalesOrderDetail
+    GROUP BY SpecialOfferID
+
+    SELECT SpecialOfferID, UnitPrice
+    FROM Sales.SalesOrderDetail
+    WHERE SpecialOfferID = 9
+
+
+-- Quero saber quanto de cada produto foi vendido hoje
+
+    SELECT ProductID, COUNT(ProductID) as 'CONTAGEM'
+    FROM Sales.SalesOrderDetail
+    GROUP BY ProductID
+
+
+-- Quero saber quantos nomes de cada nome temos cadastrados
+
+    SELECT FirstName, COUNT(FirstName) as 'CONTAGEM' 
+    FROM Person.Person
+    GROUP BY FirstName
+
+
+-- Na tabela Production.Product, quero saber a média de preço para os produtos que são pratas(silver)
+
+    SELECT color, AVG(ListPrice) as 'preco'
+    FROM Production.Product
+    WHERE Color = 'Silver'
+    GROUP BY Color
+
+-- DESAFIO 16
+/*
+Quantas pessoas tem o mesmo MiddleName agrupadas pelo MiddleName?
+(Person.Person)
+*/
+
+    SELECT MiddleName, COUNT(MiddleName) as 'Quantidade'
+    FROM Person.Person
+    GROUP BY MiddleName
+
+
+-- DESAFIO 17
+/*
+Em média, qual é a quantidade(quantity) que cada produto é vendido na loja?
+(Sales.SalesOrderDetail)
+*/
+
+    Select OrderQty, avg(OrderQty) as 'Media'
+    FROM Sales.SalesOrderDetail
+    Group by ProductID
 
