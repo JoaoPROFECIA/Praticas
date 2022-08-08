8e42e4d02c23bc6a50441c37de7d4f5563133e5a
Joao Victor <victorjv3288@gmail.com>
2022-08-08 15:02:17 -0300

    Update API_cotacao.py

--- Python-and-Json/API_cotacao.py
+++ Python-and-Json/API_cotacao.py
@@ -2,9 +2,10 @@ import requests, os, json
 
 # AwesomeAPI: https://docs.awesomeapi.com.br/api-de-moedas
 
+# API de cotação de moedas
+
 requisicao = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
 requisicao_json = requisicao.json()
-
 
 # salvar arquivo em formato json
 
@@ -13,14 +14,42 @@ SAVE_TO = os.path.join(BASE_DIR, 'cotacao.json')
 
 with open(SAVE_TO, 'w') as file:
     json.dump(requisicao_json, file, indent=2)
+    print('Arquivo cotacao.json salvo com sucesso!')
 
+dic_requisicao = requisicao_json
 
-dic_requisicao = requisicao.json()
+#  print(dic_requisicao['USDBRL']['name'],': R$',dic_requisicao['USDBRL']['bid'].replace('.',','))
+#  print(dic_requisicao['EURBRL']['name'],': R$',dic_requisicao['EURBRL']['bid'].replace('.',','))
+#  print(dic_requisicao['BTCBRL']['name'],': R$',dic_requisicao['BTCBRL']['bid'])
 
-if requisicao.status_code == 200:
-    print(dic_requisicao['USDBRL']['name'],': R$',dic_requisicao['USDBRL']['bid'].replace('.',','))
-    print(dic_requisicao['EURBRL']['name'],': R$',dic_requisicao['EURBRL']['bid'].replace('.',','))
-    print(dic_requisicao['BTCBRL']['name'],': R$',dic_requisicao['BTCBRL']['bid'])
-    
-if requisicao.status_code == 404:
-    print('Erro 404. Moeda não encontrada')
+while True:
+    print('\n')
+    print('1 - USD')
+    print('2 - EUR')
+    print('3 - BTC')
+    print('4 - Sair')
+    print('\n')
+
+    opcao = int(input('Digite a opção desejada: '))
+
+    if opcao == 1:
+        print('\n')
+        print('R$',dic_requisicao['USDBRL']['bid'].replace('.',','))
+
+    elif opcao == 2:
+        print('\n')
+        print('R$',dic_requisicao['EURBRL']['bid'].replace('.',','))
+
+    elif opcao == 3:
+        print('\n')
+        print('R$',dic_requisicao['BTCBRL']['bid'].replace('.',','))
+
+    elif opcao == 4:
+        print('\n')
+        print('Saindo...')
+        exit()
+    else:
+        print('\n')
+        print('Opção inválida!')
+        print('\n')
+        continue
