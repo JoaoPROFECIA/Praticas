26c6bfa79241d3411462b6c68a5641bcb3469d1a
Joao Victor <victorjv3288@gmail.com>
2022-08-08 11:20:59 -0300

    Update API_cotacao.py

--- Python-and-Json/API_cotacao.py
+++ Python-and-Json/API_cotacao.py
@@ -3,12 +3,20 @@ import requests
 # AwesomeAPI: https://docs.awesomeapi.com.br/api-de-moedas
 def cotacao():
     requisicao = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
+
     requisicao
+    
     requisicao.json()
+
     dic_requisicao = requisicao.json()
+
     if requisicao.status_code == 200:
-        print(dic_requisicao['USDBRL']['bid'])
+        print(dic_requisicao['USDBRL']['name'],': R$',dic_requisicao['USDBRL']['bid'].replace('.',','))
+        print(dic_requisicao['EURBRL']['name'],': R$',dic_requisicao['EURBRL']['bid'].replace('.',','))
+        print(dic_requisicao['BTCBRL']['name'],': R$',dic_requisicao['BTCBRL']['bid'])
+        
     if requisicao.status_code == 404:
         print('Erro 404. Moeda não encontrada')
     
+
 cotacao()
