5f44389585a5f9f79ce18732f5d50f76559a6e5d
Joao Victor <victorjv3288@gmail.com>
2022-08-08 19:58:11 -0300

    Update API_cotacao.py

--- Python-and-Json/API_cotacao.py
+++ Python-and-Json/API_cotacao.py
@@ -1,4 +1,4 @@
-import requests, os, json
+import requests, os, json, time
 
 #  AwesomeAPI: https://docs.awesomeapi.com.br/api-de-moedas
 
@@ -9,46 +9,61 @@ requisicao_json = requisicao.json() #  Converte para json
 
 #  salvar arquivo em formato json
 
+#  dict de cores 
+cor = {
+        'verde': '\033[32m',
+        'vermelho': '\033[31m',
+        'amarelo': '\033[33m',
+        'azul': '\033[34m',
+        'branco': '\033[37m',
+        'limpa': '\033[m'
+        }
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__)) #  diretorio atual
 SAVE_TO = os.path.join(BASE_DIR, 'cotacao.json') #  diretorio e nome do arquivo
 
 with open(SAVE_TO, 'w') as file: #  abre o arquivo
-    json.dump(requisicao_json, file, indent=2) #  salva o arquivo
-    print('Arquivo cotacao.json salvo com sucesso!') #  mensagem de sucesso
+    json.dump(requisicao_json, file, indent=4) #  salva o arquivo
+    print(cor['verde'],'Arquivo "cotacao.json" salvo com sucesso!',cor['limpa']) #  mensagem de sucesso
+    time.sleep(1)
 
-#  print(dic_requisicao['USDBRL']['name'],': R$',dic_requisicao['USDBRL']['bid'].replace('.',','))
-#  print(dic_requisicao['EURBRL']['name'],': R$',dic_requisicao['EURBRL']['bid'].replace('.',','))
-#  print(dic_requisicao['BTCBRL']['name'],': R$',dic_requisicao['BTCBRL']['bid'])
+data_e_hora_em_texto = time.strftime('%d/%m/%Y %H:%M:%S') #  data e hora atual
+
+print()
+print('Escoha uma moeda para ver a cotação:')
+print('1 - USD\n''2 - EUR\n''3 - BTC\n''4 - Sair\n\n'
+      'Cotação atualizada em:', data_e_hora_em_texto)
+print()
 
 while True: 
-    #  menu de opções
-    print('\n')
-    print('1 - USD')
-    print('2 - EUR')
-    print('3 - BTC')
-    print('4 - Sair')
-    print('\n')
+    time.sleep(0.4)
+    opcao = input('Digite a opção desejada: ').strip().upper()
+    time.sleep(0.4)
 
-    opcao = int(input('Digite a opção desejada: ')) 
+    if opcao == '1' or opcao == 'USD' or opcao == 'DOLAR' or opcao == 'DÓLAR': 
+        print()
+        print(requisicao_json['USDBRL']['name'],'\nR$',requisicao_json['USDBRL']['bid'].replace('.',',')) #  exibe o valor do dolar
+        print()
 
-    if opcao == 1: 
-        print('\n')
-        print('R$',requisicao_json['USDBRL']['bid'].replace('.',',')) #  exibe o valor do dolar
+    elif opcao == '2' or opcao == 'EUR' or opcao == 'EURO':
+        print()
+        print(requisicao_json['EURBRL']['name'],'\nR$',requisicao_json['EURBRL']['bid'].replace('.',',')) #  exibe o valor do euro
+        print()
 
-    elif opcao == 2:
-        print('\n')
-        print('R$',requisicao_json['EURBRL']['bid'].replace('.',',')) #  exibe o valor do euro
+    elif opcao == '3' or opcao == 'BTC' or opcao == 'BITCOIN':
+        print()
+        print(requisicao_json['BTCBRL']['name'],'\nR$',requisicao_json['BTCBRL']['bid']) #  exibe o valor do bitcoin
+        print()
 
-    elif opcao == 3:
-        print('\n')
-        print('R$',requisicao_json['BTCBRL']['bid'].replace('.',',')) #  exibe o valor do bitcoin
-
-    elif opcao == 4:
-        print('\n')
-        print('Saindo...') #  mensagem de saída
+    elif opcao == '4' or opcao == 'SAIR':
+        print()
+        print('Saindo',end='') #  mensagem de saída
+        for i in range(3):
+            print('.',end='')
+            time.sleep(0.4)
         exit()
     else:
-        print('\n')
-        print('Opção inválida!') #  mensagem de opção inválida
-        print('\n')
+        print()
+        print(cor['vermelho'],'Opção inválida!',cor['limpa']) #  mensagem de opção inválida
+        print()
         continue
