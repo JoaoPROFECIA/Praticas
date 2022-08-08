9be475e65667c1ed8f97c2a60aaf53cbb922ff17
Joao Victor <victorjv3288@gmail.com>
2022-08-08 14:38:23 -0300

    Update API_cotacao.py

--- Python-and-Json/API_cotacao.py
+++ Python-and-Json/API_cotacao.py
@@ -1,27 +1,26 @@
-import requests
+import requests, os, json
 
 # AwesomeAPI: https://docs.awesomeapi.com.br/api-de-moedas
-def cotacao():
-    requisicao = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
 
-    requisicao
+requisicao = requests.get('https://economia.awesomeapi.com.br/last/USD-BRL,EUR-BRL,BTC-BRL')
+requisicao_json = requisicao.json()
+
+
+# salvar arquivo em formato json
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+SAVE_TO = os.path.join(BASE_DIR, 'cotacao.json')
+
+with open(SAVE_TO, 'w') as file:
+    json.dump(requisicao_json, file, indent=2)
+
+
+dic_requisicao = requisicao.json()
+
+if requisicao.status_code == 200:
+    print(dic_requisicao['USDBRL']['name'],': R$',dic_requisicao['USDBRL']['bid'].replace('.',','))
+    print(dic_requisicao['EURBRL']['name'],': R$',dic_requisicao['EURBRL']['bid'].replace('.',','))
+    print(dic_requisicao['BTCBRL']['name'],': R$',dic_requisicao['BTCBRL']['bid'])
     
-    requisicao.json()
-    
-    # salvar arquivo em formato json
-    with open('cotacao.json', 'w') as f:
-        f.write(requisicao.text)
-
-
-    dic_requisicao = requisicao.json()
-
-    if requisicao.status_code == 200:
-        print(dic_requisicao['USDBRL']['name'],': R$',dic_requisicao['USDBRL']['bid'].replace('.',','))
-        print(dic_requisicao['EURBRL']['name'],': R$',dic_requisicao['EURBRL']['bid'].replace('.',','))
-        print(dic_requisicao['BTCBRL']['name'],': R$',dic_requisicao['BTCBRL']['bid'])
-        
-    if requisicao.status_code == 404:
-        print('Erro 404. Moeda não encontrada')
-    
-
-cotacao()
+if requisicao.status_code == 404:
+    print('Erro 404. Moeda não encontrada')
